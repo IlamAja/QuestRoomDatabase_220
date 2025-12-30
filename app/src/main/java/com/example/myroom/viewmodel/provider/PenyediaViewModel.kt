ee97dfc876d3a42f255a3fe32c89d088e32269d6
Ilam Ball <informasiilam@gmail.com>
2025-12-31 00:22:08 +0700

    membuat package provider

--- app/src/main/java/com/example/myroom/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myroom/viewmodel/provider/PenyediaViewModel.kt
@@ -1,2 +1,32 @@
 package com.example.myroom.viewmodel.provider
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.myroom.repositori.ContainerApp
+import com.example.myroom.viewmodel.DetailViewModel
+import com.example.myroom.viewmodel.EntryViewModel
+import com.example.myroom.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiSiswa().container.repositoriSiswa
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): ContainerApp = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as ContainerApp)
