d86206d62d971df6d30e842c792694a42e1d6ee3
Ilam Ball <informasiilam@gmail.com>
2025-12-30 23:24:15 +0700

    membuat container app

--- app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myroom/repositori/ContainerApp.kt
@@ -1,2 +1,32 @@
 package com.example.myroom.repositori
 
+import android.app.Application
+import android.content.Context
+import com.example.myroom.repositori.RepositoriSiswa
+import com.example.myroom.repositori.OfflineRepositoriSiswa
+import com.example.myroom.room.DatabaseSiswa
+
+
+interface AppContainer {
+    val repositoriSiswa : RepositoriSiswa
+}
+class AppContainerImpl(private val context: Context) :
+    AppContainer {
+
+    override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            siswaDao = DatabaseSiswa.getDatabase(context).siswaDao()
+        )
+    }
+}
+
+class ContainerApp : Application() {
+
+
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = AppContainerImpl(this)
+    }
+}
